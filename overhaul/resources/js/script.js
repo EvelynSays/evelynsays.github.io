988dcd839be5b53fb5984182d264967903fc5ca0
Evelyn <joannaevelynevans@gmail.com>
2024-05-22 16:26:36 +0100

    Moved color change functionality from css to js, added new group of projects.

--- overhaul/resources/js/script.js
+++ overhaul/resources/js/script.js
@@ -3,22 +3,22 @@ var i;
 
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("mouseover", function() {
-    this.classList.toggle("active");
+    this.classList.add("active");
+    this.style.backgroundColor = "#2b2b2b";
     var content = this.nextElementSibling;
     if (content.style.display === "block") {
       content.style.display = "none";
+      this.style.backgroundColor = "#000000";
     } else {
       content.style.display = "block";
     }
   });
-}
 
-var groupedProjects = document.getElementsByClassName("grouped-project");
-for (i = 0; i < groupedProjects.length; i++) {
-  groupedProjects[i].addEventListener("mouseleave", function() {
-    var content = this.querySelector(".content");
+  coll[i].addEventListener("mouseleave", function() {
+    var content = this.nextElementSibling;
     if (content.style.display === "block") {
       content.style.display = "none";
+      this.style.backgroundColor = "#000000";
     }
   });
 }
