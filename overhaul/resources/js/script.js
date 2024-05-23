028e907a62115eb8097f7c49bdcbb11dd3c837bd
Evelyn <joannaevelynevans@gmail.com>
2024-05-23 12:36:24 +0100

    Added blink animations to navigation, filled out navigation links

--- overhaul/resources/js/script.js
+++ overhaul/resources/js/script.js
@@ -1,24 +1,44 @@
-var coll = document.getElementsByClassName("collapsible");
+// Collapsible project sections.
+var coll = document.getElementsByClassName("grouped-project");
 var i;
 
 for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("mouseover", function() {
-    this.classList.add("active");
-    this.style.backgroundColor = "#2b2b2b";
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-      this.style.backgroundColor = "#000000";
-    } else {
-      content.style.display = "block";
-    }
-  });
+    coll[i].addEventListener("mouseover", function() {
+        var button = this.getElementsByClassName("collapsible")[0];
+        var content = this.getElementsByClassName("content")[0];
+        button.classList.add("active");
+        button.style.backgroundColor = "#2b2b2b";
+        content.style.display = "block";
+    });
 
-  coll[i].addEventListener("mouseleave", function() {
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-      this.style.backgroundColor = "#000000";
-    }
-  });
+    coll[i].addEventListener("mouseleave", function() {
+        var button = this.getElementsByClassName("collapsible")[0];
+        var content = this.getElementsByClassName("content")[0];
+        content.style.display = "none";
+        button.style.backgroundColor = "#000000";
+    });
+}
+
+// Animated page elements in response to nav links.
+var linkToAnimateElementMap = {
+  'about-link': 'about-animate',
+  'projects-link': 'projects-animate',
+  'skills-link': 'skills-animate',
+  'contact-link': 'contact-animate'
+};
+
+for (var linkId in linkToAnimateElementMap) {
+  var link = document.getElementById(linkId);
+  var animateElementId = linkToAnimateElementMap[linkId];
+  var animateElement = document.getElementById(animateElementId);
+
+  link.addEventListener('click', (function(animateElement) {
+    return function(event) {
+      event.preventDefault(); // prevent the default action
+      animateElement.classList.add('blink');
+      setTimeout(function() {
+          animateElement.classList.remove('blink');
+      }, 2000); // remove the blink class after 2 seconds
+    };
+  })(animateElement));
 }
